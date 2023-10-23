3e88c69f75c95c285a65b5bf4b0a16a6305887a5
Mohamed Ahmed <101581652+mohamedRefaie67@users.noreply.github.com>
2023-10-23 19:07:09 +0300

    Add files via upload

---
+++ Landing Page/index.js
@@ -0,0 +1,9 @@
+function imgslider(anything){
+    document.querySelector('.Ship').src=anything;
+}
+
+
+function changecolor(color){
+    const circle=document.querySelector('.circle')
+    circle.style.background=color;
+}
